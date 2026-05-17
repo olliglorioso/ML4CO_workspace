358f27d4f480b122098d752f19e25892ad550e75
Olli Glorioso <olli.glorioso8@gmail.com>
2026-05-17 18:38:08 +0200

    parameterize amount of heads K in GAT

--- starting_kit/model_gat.py
+++ starting_kit/model_gat.py
@@ -19,9 +19,10 @@ from torch_geometric.utils import subgraph
 FEATURE_COUNT = 7
 HIDDEN_CHANNELS = 64
 NUM_LAYERS = 4
+K_H = 6
 
 class GATv2Net(nn.Module):
-    def __init__(self, in_channels=FEATURE_COUNT, hidden_channels=HIDDEN_CHANNELS, num_layers=NUM_LAYERS, heads=4):
+    def __init__(self, in_channels=FEATURE_COUNT, hidden_channels=HIDDEN_CHANNELS, num_layers=NUM_LAYERS, heads=K_H):
         super().__init__()
 
         self.convs = nn.ModuleList()
